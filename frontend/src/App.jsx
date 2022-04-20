6130c8da74a344ad0582b157fdee886830b7c65d
bdf007 <36994337+bdf007@users.noreply.github.com>
2022-04-20 10:33:47 +0200

    Revert "Background"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,14 @@
-import React from "react";
-import Header from "@components/Header";
-import { Routes, Route } from "react-router-dom";
-import Maps from "@pages/Maps";
-import Synopsis from "@pages/Synopsis";
-import Heros from "@pages/Heros";
-import Encyclopedia from "@pages/Encyclopedia";
-import Footer from "@components/Footer";
-import Background from "@components/Background.jsx";
+import Home from "@pages/Home";
 
-export default function App() {
+import "./App.css";
+
+function App() {
   return (
-    <div className="mainBody">
-      <Background />
-      <Header />
-      <Routes>
-        <Route path="/maps" element={<Maps />} />
-        <Route path="/history" element={<Synopsis />} />
-        <Route path="/heros" element={<Heros />} />
-        <Route path="/encyclopedia" element={<Encyclopedia />} />
-      </Routes>
-      <Footer />
+    <div className="App">
+      <Home />
+      <p>coucou</p>
     </div>
   );
-};
+}
+
+export default App;
--- frontend/src/pages/Maps.jsx
+++
@@ -1,7 +0,0 @@
-export default function Maps() {
-  return (
-    <div>
-      <h1>hello maps</h1>
-    </div>
-  );
-}
