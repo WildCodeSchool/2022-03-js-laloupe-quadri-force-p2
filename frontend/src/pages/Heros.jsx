84453b3b45887dbcc7cab752b39f8a2742ced787
Enzo Regnier <enzoregnier10@gmail.com>
2022-04-27 14:08:06 +0200

    scrool fixed and heros page

--- frontend/src/pages/Heros.jsx
+++ frontend/src/pages/Heros.jsx
@@ -2,8 +2,36 @@ import React from "react";
 
 const Heros = () => {
   return (
-    <div>
+    <div className="Prodige">
       <h1>hello Heroes</h1>
+      <section class="one">
+        <img src="src/assets/Mipha.png" alt="status" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, atque error totam eius culpa officia deserunt maiores nesciunt necessitatibus in dolorum, sint libero suscipit, commodi animi dolor repellendus omnis aperiam!
+          <span class="line"></span>
+        </p>
+      </section>
+      <section class="two">
+        <img src="src/assets/Daruk.png" alt="status" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, beatae! Quod impedit laboriosam dolor omnis dicta id. Accusamus in qui dolores natus eum id placeat, dolorum laudantium assumenda. Id, accusantium?
+          <span class="line"></span>
+        </p>
+      </section>
+      <section class="three">
+        <img src="src/assets/Revali.png" alt="status" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, beatae! Quod impedit laboriosam dolor omnis dicta id. Accusamus in qui dolores natus eum id placeat, dolorum laudantium assumenda. Id, accusantium?
+          <span class="line"></span>
+        </p>
+      </section>
+      <section class="four">
+        <img src="src/assets/Urbosa.png" alt="status" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, beatae! Quod impedit laboriosam dolor omnis dicta id. Accusamus in qui dolores natus eum id placeat, dolorum laudantium assumenda. Id, accusantium?
+          <span class="line"></span>
+        </p>
+      </section>
     </div>
   );
 };
