1066466866bf68e1ad5ed8b482a3458400389c26
Toupin Jean-Marie <jm.toupin@outlook.fr>
2022-04-27 11:44:22 +0200

    img-text-title

--- frontend/src/pages/Synopsis.jsx
+++ frontend/src/pages/Synopsis.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 const Synopsis = () => {
   return (
-    <div>
-      <h1>hello histoire</h1>
+    <div className="History">
+      <section className="titleHyrules">
+        <h1>Hystory Of Hyrules</h1>
+      </section>
+      <div className="synopsis">
+      <img  src="src/assets/LogoHyrule.png" />
+      <p>One hundred years after the battle that led to the death of the Prodigies. Link awakens from a long sleep in the Sanctuary of Rebirth. A female voice calls him and implores him to wake up. He no longer has any memory of his past. He meets an old man who turns out to be the spirit of King Rhoam. The latter explains to him that Zelda, locked up in Hyrule Castle, is fighting alone against Ganon, "The Scourge", and asks him to go and fight him, before he breaks free and destroys the world.
+To understand what happened a century ago, Link must call on several characters capable of telling him about the events prior to his sleep. Impa entrusts him with twelve photographs of places to find his memories. The hero travels through Hyrule and returns to the places of his past. He boards the four God Beasts and frees the souls of the four Prodigies from Ganon's shadows, allowing them to once again control their creature. After obtaining the Legendary Sword, which can be found in the Lost Woods, Link enters Hyrule Castle and defeats Ganon, "The Scourge". He then restores peace to the kingdom and allows the spirits of King Rhoam and the four Prodigies to fly away.
+
+</p>
+      </div>
     </div>
   );
 };
