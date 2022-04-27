4b2bfb730f0462725f5b3808093ac47e5a96eab9
Enzo Regnier <enzoregnier10@gmail.com>
2022-04-27 16:08:05 +0200

    page heros

--- frontend/src/pages/Synopsis.jsx
+++ frontend/src/pages/Synopsis.jsx
@@ -4,6 +4,9 @@ const Synopsis = () => {
   return (
     <div>
       <h1>hello histoire</h1>
+      <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, atque error totam eius culpa officia deserunt maiores nesciunt necessitatibus in dolorum, sint libero suscipit, commodi animi dolor repellendus omnis aperiam!
+      </p>
     </div>
   );
 };
