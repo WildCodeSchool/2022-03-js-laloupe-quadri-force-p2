014778f9225b6fdd022850344302234594f82c4d
BDF007 <bdf007@gmail.com>
2022-04-28 10:46:35 +0200

    ajout de l'appel a l'api avec le nom et la dexcription sans CSS

--- frontend/src/components/Appel.jsx
+++ frontend/src/components/Appel.jsx
@@ -1,27 +1,27 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Cards from "./Cards";
 
 const Appel = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
     axios.get("https://botw-compendium.herokuapp.com/api/v2").then((res) => {
       setData(res.data.data.materials);
-      console.warn(res.data.data.materials);
     });
-    axios
-      .get("https://zelda.fanapis.com/api/characters?limit=100")
-      .then((res) => console.warn("API ZELDA/characters : ", res.data));
-    axios
-      .get("https://zelda.fanapis.com/api/items")
-      .then((res) => console.warn("API ZELDA/items : ", res.data));
-    axios
-      .get("https://zelda.fanapis.com/api/places?limit=100")
-      .then((res) => console.warn("API ZELDA/places : ", res.data));
+    // axios
+    //   .get("https://zelda.fanapis.com/api/characters?limit=100")
+    //   .then((res) => console.warn("API ZELDA/characters : ", res.data));
+    // axios
+    //   .get("https://zelda.fanapis.com/api/items")
+    //   .then((res) => console.warn("API ZELDA/items : ", res.data));
+    // axios
+    //   .get("https://zelda.fanapis.com/api/places?limit=100")
+    //   .then((res) => console.warn("API ZELDA/places : ", res.data));
   }, []);
   return (
     <div>
-      {data.map((monster, index) => (
-        <img src={monster.image} alt={index} width="100px" />
+      {data.map((monster) => (
+        <Cards key={`monster-${monster.id}`} monster={monster} />
       ))}
     </div>
   );
