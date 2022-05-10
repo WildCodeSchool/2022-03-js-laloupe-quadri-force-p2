81f1b5cea66e58e1024d8a1929f6132e98f825dd
BDF007 <bdf007@gmail.com>
2022-05-10 10:58:44 +0200

    ajout de l'ecran noir lorsque l'ecran est en mode portrait

---
+++ src/components/BlackPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const BlackPage = () => {
+  return (
+    <div className="BlackPage">
+      <h1>Please use the landscape mode</h1>
+      <img
+        src="src/assets/bouton_on_off.png"
+        alt="bouton on/off"
+        className="BlackBouton"
+      />
+    </div>
+  );
+};
+
+export default BlackPage;
